975efcfabf188621a7a4f9c70a0cf99306ab48a6
Christophe Coevoet <stof@notk.org>
2016-11-11 09:53:36 +0100

    Remove the array HTML escaping
    Escaping must be performed on the rendered string. It does not make sense
    to escape each array value separately.

--- web-fixtures/utils.php
+++ web-fixtures/utils.php
@@ -2,17 +2,7 @@
 
 function html_escape_value($data)
 {
-    if (!is_array($data)) {
-        return htmlspecialchars($data, ENT_QUOTES, 'UTF-8', false);
-    }
-
-    $escapedData = array();
-
-    foreach ($data as $key => $value) {
-        $escapedData[html_escape_value($key)] = html_escape_value($value);
-    }
-
-    return $escapedData;
+    return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
 }
 
 /**
