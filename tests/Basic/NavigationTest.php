3701a8a668b3d6709967ba32e4076c1bc1bf29fe
Andrew Nicols <andrew@nicols.co.uk>
2021-01-12 07:58:41 +0800

    Add test for geckodriver#653 (#3)

--- tests/Basic/NavigationTest.php
+++ tests/Basic/NavigationTest.php
@@ -83,4 +83,17 @@ class NavigationTest extends TestCase
 
         $this->assertEquals($this->pathTo('/links.html?quoted'), $session->getCurrentUrl());
     }
+
+    public function testBlockLevelElementInAnchor()
+    {
+        $this->getSession()->visit($this->pathTo('/links.html'));
+        $page = $this->getSession()->getPage();
+        $link = $page->findLink('Link in a block element');
+
+        $this->assertNotNull($link);
+        $this->assertRegExp('/basic_form\.html$/', $link->getAttribute('href'));
+        $link->click();
+
+        $this->assertEquals($this->pathTo('/basic_form.html'), $this->getSession()->getCurrentUrl());
+    }
 }
